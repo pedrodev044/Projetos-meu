d5b51a7acbefd1e7b686cc3a76ee6a6e238ad588
Pedro Arthur <pedroarthurcavalcanti08@gmail.com>
2026-04-15 08:48:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import{ CalculadoraComponent} from "./calculadora/calculadora.component"
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/page/page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page',
+  imports: [],
+  templateUrl: './page.component.html',
+  styleUrl: './page.component.scss'
+})
+export class PageComponent {
+
+}
